5d7a341ad4a1006ca8084183d2478918c0d5cf91
niuiic <niuiicde@gmail.com>
2025-02-11 09:42:07 +0800

    chore: eslint

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('Hello')
+</script>
+
 <template>
   <view class="content">
     <image class="logo" src="/static/logo.png" />
@@ -6,12 +12,6 @@
     </view>
   </view>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const title = ref('Hello')
-</script>
 
 <style>
 .content {
