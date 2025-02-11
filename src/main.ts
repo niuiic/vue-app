0bf998ee51f8825bce599b3020f6e4a0d1211272
niuiic <niuiicde@gmail.com>
2025-02-11 09:17:06 +0800

    chore: add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 
-export function createApp() {
-  const app = createSSRApp(App)
-  return { app }
-}
+export const createApp = () => createSSRApp(App)
